690bf524971b8ddb8b7fdf87122e5a0cbb0bb612
Yaroslav Kosovych <bassicinbox@gmail.com>
2023-12-20 21:48:52 +0200

    add react game

--- other/road/js/main.js
+++
@@ -1,9 +0,0 @@
-$('#header-menu-btn').on('click', function () {
-	$('#header-menu-btn').toggleClass('active');
-	$('#header-nav').toggleClass('active');
-});
-
-$('.asphalt-type__label').on('click', function (ev) {
-	$('.asphalt-type__label').removeClass('active');
-	$(this).addClass('active');
-});
